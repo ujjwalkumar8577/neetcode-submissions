222fe7e781922e0b0a8c6c4aeae6ddcfe794398f
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2026-05-25 23:38:12 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    int dp[];
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        return climbStairsHelper(n);
+    }
+
+    public int climbStairsHelper(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        return dp[n] = climbStairsHelper(n-1) + climbStairsHelper(n-2);
+    }
+}
